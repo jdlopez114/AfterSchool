02e06a3c7cb0529a48905e19e6090073089982c4
jdlopez114 <jdlopez114@gmail.com>
2017-01-27 20:43:46 -0500

    switch statement

--- app/src/main/java/com/example/jello/afterschool/MainActivity.java
+++ app/src/main/java/com/example/jello/afterschool/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.annotation.ColorRes;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
@@ -11,12 +12,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 
+import com.example.jello.afterschool.fragments.HomeFragment;
+import com.example.jello.afterschool.fragments.SearchFragment;
+import com.example.jello.afterschool.fragments.StudentsFragment;
+
 public class MainActivity extends AppCompatActivity {
 
-    private static final String SELECTED_ITEM = "arg_selected_item";
+    private static final String SELECTED_ITEM = "selected.item";
 
     private BottomNavigationView bottomBar;
-    private int selectedItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,88 +28,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         bottomBar = (BottomNavigationView) findViewById(R.id.navigation);
-
         bottomBar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
                 selectFragment(item);
                 return true;
             }
         });
-
-        MenuItem menuItem;
-
-        if (savedInstanceState != null) {
-            selectedItem = savedInstanceState.getInt(SELECTED_ITEM, 0);
-            menuItem = bottomBar.getMenu().findItem(selectedItem);
-        } else {
-            menuItem = bottomBar.getMenu().getItem(0);
-        }
-        selectFragment(menuItem);
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        outState.putInt(SELECTED_ITEM, selectedItem);
-        super.onSaveInstanceState(outState);
-    }
-
-    @Override
-    public void onBackPressed() {
-        MenuItem homeItem = bottomBar.getMenu().getItem(0);
-        if (selectedItem != homeItem.getItemId()) {
-            // select home item
-            selectFragment(homeItem);
-        } else {
-            super.onBackPressed();
-        }
     }
 
     private void selectFragment(MenuItem item) {
-        Fragment frag = null;
-        // init corresponding fragment
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
         switch (item.getItemId()) {
             case R.id.menu_home:
-                frag = MenuFragment.newInstance(getString(R.string.menu_home),
-                        getColorFromRes(R.color.color_home));
+//                FragmentManager fragmentManager = getSupportFragmentManager();
+//                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.add(R.id.container, new SearchFragment());
+                fragmentTransaction.commit();
                 break;
             case R.id.menu_search:
-                frag = MenuFragment.newInstance(getString(R.string.menu_search),
-                        getColorFromRes(R.color.color_search));
+//                FragmentManager fragmentManager = getSupportFragmentManager();
+//                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.add(R.id.container, new HomeFragment());
+                fragmentTransaction.commit();
                 break;
             case R.id.menu_students:
-                frag = MenuFragment.newInstance(getString(R.string.menu_students),
-                        getColorFromRes(R.color.color_students));
+//                FragmentManager fragmentManager = getSupportFragmentManager();
+//                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.add(R.id.container, new StudentsFragment());
+                fragmentTransaction.commit();
                 break;
         }
-
-        // update selected item
-        selectedItem = item.getItemId();
-
-        // uncheck the other items.
-        for (int i = 0; i < bottomBar.getMenu().size(); i++) {
-            MenuItem menuItem = bottomBar.getMenu().getItem(i);
-            menuItem.setChecked(menuItem.getItemId() == item.getItemId());
-        }
-
-        updateToolbarText(item.getTitle());
-
-        if (frag != null) {
-            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-            ft.add(R.id.activity_main, frag, frag.getTag());
-            ft.commit();
-        }
-    }
-
-    private void updateToolbarText(CharSequence text) {
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setTitle(text);
-        }
-    }
-
-    private int getColorFromRes(@ColorRes int resId) {
-        return ContextCompat.getColor(this, resId);
     }
 }
